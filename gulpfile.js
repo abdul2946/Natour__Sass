daf9e7bc8873e2b8c91701cb1e80682b22944d62
abdul2946 <aazim3510@gmail.com>
2024-09-03 13:00:22 +0530

    minor change

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
 import gulp from "gulp";
 import autoPrefixer from "gulp-autoprefixer";
+import autoprefixer from "autoprefixer";
 import gulpSass from "gulp-sass";
 import minify from "gulp-clean-css"
 import sass from "node-sass"
+import browserslist from "browserslist";
 
 const scss = gulpSass(sass)
+const prefix = autoPrefixer(autoprefixer);
 
 gulp.task("compilescss", async function () {
-	gulp.src("./sass/**/*.scss").pipe(await scss()).pipe(await autoPrefixer()).pipe(await gulp.dest("./css"))
+	gulp
+		.src("./sass/**/*.scss")
+		.pipe(await scss())
+		.pipe(
+			await autoPrefixer({
+				cascade: false,
+			})
+		)
+		.pipe(await gulp.dest("./css"));
 });
 
